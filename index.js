d47ee788dccd62b223aa14960d215f9a35b6cb5d
Julien Landuré <julien.landure@gmail.com>
2023-10-05 22:10:39 +0200

    :sparkles: shortvid generation

--- index.js
+++ index.js
@@ -1,53 +1,52 @@
-const file = require("./event-tz-nantes-2021-2.json");
-const getTag = (id) => {
-  const item = file.categories.find((cat) => cat.id === id);
-  return (item && item.name) || "Unknown";
-};
+const file = require("./devfest2023.json");
+
+const stringForGeneratingShortVid = `
+pnpm remotion render remotion/index.tsx DevfestNantesTalkLoop out/${outTitle}.mp4 --props='{"title":"${title}","speakers":${speakers},"date":"${dateStart}","time":"${timeStart}","location":"${track}"}'
+`
+const stringForSpeaker = [{"name":"Mickaël Alves","picture":"https://pbs.twimg.com/profile_images/1635742650090283013/9qflRryB_400x400.jpg"},{"name":"Antoine Caron","picture":"https://ca.slack-edge.com/T108ZKPMF-U5M2F1A69-36e4b6574de8-192"}]
+
+var outTitle = "modern-web-debugging-with-devt" // id
+var title = "🇺🇸Modern Web Debugging with DevTools" // title
+var speakers = JSON.stringify(stringForSpeaker) // speakerIds
+var dateStart = "19 Octobre" //dateStart
+var timeStart = "15:00" // dateStart
+var track = "Jules Verne" // trackId
+
+const getSpeaker = (id) => {
+  const speaker = file.speakers.find((speaker) => speaker.id === id);
+  return {name: speaker.name.replace("\'", "'\\''"), picture: speaker.photoUrl || "TODO"}
+}
 
 const getTrack = (id) => {
-  const item = file.tracks.find((track) => track.id === id);
-  return (item && item.room) || "Unknown";
-};
+  const track = file.event.tracks.find((track) => track.id === id);
+  return track.name.replace("\'", "'\\''")
+}
 
-const sessions = {};
-file.talks = file.talks.map(talk => {
-  const startTime = "2021-09-24T"+talk.startTime.replace("h",":")+":00+02:00"
-  const endTime = new Date(new Date(startTime).getTime() + 25*60000);
-  return {
-    ...talk,
-    startTime,
-    endTime: "2021-09-24T"+endTime.getHours().toString().padStart(2,'0')+":"+endTime.getMinutes()+":00+02:00"
-  };
+const shortVidSessions = file.sessions.map(session => {
+  //if(session.id !== "-la-checklist-ultime-pour-rend") return 
+  const sessionOutTitle = session.id
+  const sessionTitle = session.title.replace("\'", "'\\''")
+  const sessionSpeakers = session.speakerIds.map(speakerId => {
+    return getSpeaker(speakerId);
+  })
+  var sessionDateStart
+  if(session.dateStart.startsWith("2023-10-20")) {
+    sessionDateStart = "20 Octobre"
+  } else if(session.dateStart.startsWith("2023-10-19")) {
+    sessionDateStart = "19 Octobre"
+  }
+  const dateStart = new Date(session.dateStart)
+  const sessionTimeStart = dateStart.getHours() + "h" + dateStart.getMinutes().toString().padStart(2, '0')
+  const sessionTrack = getTrack(session.trackId)
+  return {sessionOutTitle, sessionTitle, sessionSpeakers, sessionDateStart, sessionTimeStart, sessionTrack}
 })
-file.talks.sort((t1,t2)=> new Date(t1.startTime) - new Date(t2.startTime))
-file.talks.forEach((talk, index) => {
-  sessions[index] = {
-    title: talk.title,
-    description: talk.abstract,
-    tags: [getTag(talk.categories)],
-    speakers: talk.speakers,
-    id: index,
-    trackTitle: talk.track,
-    startTime: talk.startTime,
-    endTime: talk.endTime
-    // startTime: "2019-04-23T08:30:00+02:00",
-    // trackTitle: "Zenika",
-  };
-});
+for (session of shortVidSessions) {
+  // console.log(`
+  // pnpm remotion render remotion/index.tsx DevfestNantesTalkLoop out/${outTitle}.mp4 --props='{"title":"${title}","speakers":${speakers},"date":"${dateStart}","time":"${timeStart}","location":"${track}"}'
+  // `);
+  console.log(`touch out/${session.sessionOutTitle}.info
+  
+  `);
+  // && pnpm remotion render remotion/index.tsx DevfestNantesTalkLoop out/${session.sessionOutTitle}.mp4 --props='{"title":"${session.sessionTitle}","speakers":${JSON.stringify(session.sessionSpeakers)},"date":"${session.sessionDateStart}","time":"${session.sessionTimeStart}","location":"${session.sessionTrack}"}'
+}
 
-
-
-//"startTime":"2021-04-23T09:30:00+02:00","endTime":"2021-04-23T10:30:00+02:00","trackTitle":"Nantes"
-
-const speakers = {};
-file.speakers.forEach((speaker, index) => {
-  speakers[speaker.uid] = {
-    company: "Zenika",
-    name: speaker.displayName,
-    photoUrl: speaker.photoURL,
-    socials: [],
-    id: speaker.uid,
-  };
-});
-
-console.log(JSON.stringify({ sessions, speakers }, null, true));
